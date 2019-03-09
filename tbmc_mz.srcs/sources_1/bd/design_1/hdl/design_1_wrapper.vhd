b2d3ab257a7b5b2535e6dd6828b51041368c9566
Oleg V <oleg@localhost.localdomain>
2019-03-09 21:54:51 +0300

    Updated exported hardware

--- tbmc_mz.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ tbmc_mz.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
---Date        : Sat Mar  9 12:32:42 2019
+--Date        : Sat Mar  9 20:12:09 2019
 --Host        : localhost.localdomain running 64-bit unknown
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
