95f16d48dc83a623a16028e7bc542582eaeb6428
Oleg V <oleg@localhost.localdomain>
2019-03-21 00:17:27 +0300

    Missing SDI inputs pullups added

--- tbmc_mz.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ tbmc_mz.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
---Date        : Mon Mar 18 12:54:57 2019
+--Date        : Mon Mar 18 13:36:56 2019
 --Host        : localhost.localdomain running 64-bit unknown
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
